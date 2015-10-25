261b16bd48b084a9d2cb28e3735447eabcf555cb
MilkVetch <457804000@qq.com>
2015-10-25 17:15:18 +1300

    finish menu btn

--- js/main.js
+++ js/main.js
@@ -11,16 +11,37 @@
 			$('.loading').css('display','none');
 			$('.loaded').css('display','block');
 			$('header').css('display','block');
+
+			$('#navigation-button').addClass('menu-tran');
 			setTimeout(function(){
-				$('#navigation-button').addClass('loaded');
-				$('.welcome-pokemon span').addClass('welcome-loaded');			
-			},10);
+				$('#navigation-button').addClass('menu-loaded');
+				$('.welcome-pokemon span').addClass('welcome-loaded');	
+			},50);
 		},3000);
+		setTimeout(function(){
+			$('#navigation-button').removeClass('menu-tran menu-loaded');
+		},4200);
 	}
 // Menu Click
-	var menuClick = 0;
+	var clickNumber = 0;
 	$('#navigation-button').click(function(){
 		$('.nav-top').toggleClass('small-screen');
+		setTimeout(function(){
+			$('#navigation-button').addClass('menu-tran');
+			if (clickNumber === 0){
+				$('#navigation-button').css({transform: 'rotate(360deg)'});
+				clickNumber = 1;
+			} else {
+				$('#navigation-button').css({transform: 'rotate(0)'});
+				clickNumber = 0;
+			}
+			$('#navigation-button').toggleClass('menu-click');
+		},50);
 	});
+
 // ScrollBar Animation
-	
+	$(window).scroll(function(){
+		$('#navigation-button').removeClass('menu-tran');
+		var wScroll = $(this).scrollTop()*5;
+		$('#navigation-button').css('transform', 'rotate('+ wScroll +'deg)');
+	});
